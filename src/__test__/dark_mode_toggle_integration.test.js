f345d1fee373531f3bc30db10319ada70d81baf2
hhuang2088 <hhuang2088@gmail.com>
2019-06-27 17:53:51 -0700

    rewrote tests to reflect structure of integration tests

--- src/__test__/dark_mode_toggle_integration.test.js
+++ src/__test__/dark_mode_toggle_integration.test.js
@@ -4,6 +4,7 @@ import Root from '../Root';
 import App from '../components/App';
 
 let wrapper;
+let darkModeButton;
 
 beforeEach(() => {
 	wrapper = mount(
@@ -11,31 +12,24 @@ beforeEach(() => {
 			<App />
 		</Root>
 	);
+	darkModeButton = wrapper.find('#dark-mode-toggle');
 });
 
 afterEach(() => {
 	wrapper.unmount();
 });
 
-describe('the #dark-mode-toggle element', () => {
-	let darkModeButton;
+it('elements in the app start off without the inverted class', () => {
+	expect(wrapper.find('.inverted').length).toEqual(0);
+});
 
-	beforeEach(() => {
-		darkModeButton = wrapper.find('#dark-mode-toggle');
-	});
+it('clicking the #dark-mode-toggle element after it has already been clicked removes the inverted class', () => {
+	darkModeButton.simulate('click');
+	darkModeButton.simulate('click');
+	expect(wrapper.find('.inverted').length).toEqual(0);
+});
 
-	it('elements in the app start off without the inverted class', () => {
-		expect(wrapper.find('.inverted').length).toEqual(0);
-	});
-
-	it('clicking the #dark-mode-toggle element after it has already been clicked removes the inverted class', () => {
-		darkModeButton.simulate('click');
-		darkModeButton.simulate('click');
-		expect(wrapper.find('.inverted').length).toEqual(0);
-	});
-
-	it('clicking the #dark-mode-toggle element adds the inverted class to elements in the app', () => {
-		darkModeButton.simulate('click');
-		expect(wrapper.find('.inverted').length).toBeGreaterThanOrEqual(1);
-	});
+it('clicking the #dark-mode-toggle element adds the inverted class to elements in the app', () => {
+	darkModeButton.simulate('click');
+	expect(wrapper.find('.inverted').length).toBeGreaterThanOrEqual(1);
 });
