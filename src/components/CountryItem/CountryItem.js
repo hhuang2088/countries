41faf240ae63dd7a0d1b30af1e545915ea0f92f9
hhuang2088 <hhuang2088@gmail.com>
2019-07-13 00:57:28 -0700

    if country prop is empty, the app redirects to index page

--- src/components/CountryItem/CountryItem.js
+++ src/components/CountryItem/CountryItem.js
@@ -4,44 +4,59 @@ import { Link } from 'react-router-dom';
 import { selectCountry, fetchCountriesByCode } from '../../actions/';
 
 class CountryItem extends React.Component {
-	numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
+  numberWithCommas = x => {
+    if (x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+  };
 
-	onClick = () => {
-		this.props.fetchCountriesByCode(this.props.country.borders);
-		this.props.selectCountry(this.props.country);
-	}
+  onClick = () => {
+    this.props.fetchCountriesByCode(this.props.country.borders);
+    this.props.selectCountry(this.props.country);
+  };
 
-	render() {
-		return(
-			<div className='column'>
-				<div className={`ui segment grey card ${this.props.display}`}>
-					<div className="image">
-						<Link to={`/show/${this.props.country.numericCode}`} onClick={this.onClick}>
-							<img
-									className="ui large image"
-									src={this.props.country.flag}
-									alt={this.props.country.name}
-							/>
-						</Link>
-					</div>
-					<div className={`content`}>
-						<h3>{this.props.country.name}</h3>
-						<p><b>Population:</b> {this.numberWithCommas(this.props.country.population)}</p>
-						<p><b>Region:</b> {this.props.country.region}</p>
-						<p><b>Capital:</b> {this.props.country.capital}</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="column">
+        <div className={`ui segment grey card ${this.props.display}`}>
+          <div className="image">
+            <Link
+              to={`/show/${this.props.country.numericCode}`}
+              onClick={this.onClick}
+            >
+              <img
+                className="ui large image"
+                src={this.props.country.flag}
+                alt={this.props.country.name}
+              />
+            </Link>
+          </div>
+          <div className={`content`}>
+            <h3>{this.props.country.name}</h3>
+            <p>
+              <b>Population:</b>{' '}
+              {this.numberWithCommas(this.props.country.population)}
+            </p>
+            <p>
+              <b>Region:</b> {this.props.country.region}
+            </p>
+            <p>
+              <b>Capital:</b> {this.props.country.capital}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		display: state.interface.display
-	}
-}
+const mapStateToProps = state => {
+  return {
+    display: state.interface.display,
+  };
+};
 
-export default connect(mapStateToProps, { selectCountry, fetchCountriesByCode })(CountryItem);
+export default connect(
+  mapStateToProps,
+  { selectCountry, fetchCountriesByCode }
+)(CountryItem);
--- src/components/CountryShow/CountryShow.js
+++ src/components/CountryShow/CountryShow.js
@@ -5,85 +5,136 @@ import { fetchCountriesByCode, selectCountry } from '../../actions/';
 import CountryLink from '../CountryLink/CountryLink';
 
 class CountryShow extends React.Component {
-	renderBorderLinks = () => {
-		return(
-			this.props.borderCountries.map((country) => {
-				return (
-					<CountryLink country={country} key={country.numericCode} />
-				);
-			})
-		);
-	}
+  renderBorderLinks = () => {
+    return this.props.borderCountries.map(country => {
+      return <CountryLink country={country} key={country.numericCode} />;
+    });
+  };
 
-	numberWithCommas = (x) => {
-		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
+  numberWithCommas = x => {
+    if (x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+  };
 
-	joinCurrencies = (currencies) => {
-		const currenciesString = [];
-		currencies.forEach((currency) => {
-			currenciesString.push(currency.code);
-		});
-		return(currenciesString.join(', '));
-	}
+  joinCurrencies = currencies => {
+    if (currencies) {
+      const currenciesString = [];
+      currencies.forEach(currency => {
+        currenciesString.push(currency.code);
+      });
+      return currenciesString.join(', ');
+    }
+  };
 
-	joinLanguages = (languages) => {
-		const languagesString = [];
-		languages.forEach((language) => {
-			languagesString.push(language.name);
-		});
-		return(languagesString.join(', '));
-	}
+  renderInfo = () => {
+    if (this.props.country.name) {
+      return (
+        <div className={`ui basic segment ${this.props.display}`}>
+          <div className="ui two column grid container">
+            <div className="row">
+              <Link to="/" className="ui button">
+                Back
+              </Link>
+            </div>
+            <div className="row">
+              <div className="column">
+                <img
+                  className="ui huge image"
+                  src={this.props.country.flag}
+                  alt={this.props.country.name}
+                />
+              </div>
+              <div className="column">
+                <h2>{this.props.country.name}</h2>
+                <div className="ui two column grid">
+                  <div className="row">
+                    <div className="column">
+                      <p>
+                        <b>Native Name:</b> {this.props.country.nativeName}
+                      </p>
+                      <p>
+                        <b>Population:</b>{' '}
+                        {this.numberWithCommas(this.props.country.population)}
+                      </p>
+                      <p>
+                        <b>Region:</b> {this.props.country.region}
+                      </p>
+                      <p>
+                        <b>Sub Region:</b> {this.props.country.subregion}
+                      </p>
+                      <p>
+                        <b>Capital:</b> {this.props.country.capital}
+                      </p>
+                    </div>
+                    <div className="column">
+                      <p>
+                        <b>Top Level Domain:</b>{' '}
+                        {this.props.country.topLevelDomain.join(', ')}
+                      </p>
+                      <p>
+                        <b>Currencies:</b>{' '}
+                        {this.joinCurrencies(this.props.country.currencies)}
+                      </p>
+                      <p>
+                        <b>Languages:</b>{' '}
+                        {this.joinLanguages(this.props.country.languages)}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="ui grid container">
+                      <p>
+                        <b>Border Countries: </b>
+                      </p>
+                      {this.renderBorderLinks()}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
 
-	render() {
-		return(
-			<div className={`ui basic segment ${this.props.display}`}>
-				<div className="ui two column grid container">
-					<div className="row">
-						<Link to="/" className="ui button">Back</Link>
-					</div>
-					<div className="row">
-						<div className="column">
-							<img className="ui huge image" src={this.props.country.flag} alt={this.props.country.name}/>
-						</div>
-						<div className="column">
-							<h2>{this.props.country.name}</h2>
-							<div className="ui two column grid">
-								<div className="row">
-									<div className="column">
-										<p><b>Native Name:</b> {this.props.country.nativeName}</p>
-										<p><b>Population:</b> {this.numberWithCommas(this.props.country.population)}</p>
-										<p><b>Region:</b> {this.props.country.region}</p>
-										<p><b>Sub Region:</b> {this.props.country.subregion}</p>
-										<p><b>Capital:</b> {this.props.country.capital}</p>
-									</div>
-									<div className="column">
-										<p><b>Top Level Domain:</b> {this.props.country.topLevelDomain.join(', ')}</p>
-										<p><b>Currencies:</b> {this.joinCurrencies(this.props.country.currencies)}</p>
-										<p><b>Languages:</b> {this.joinLanguages(this.props.country.languages)}</p>
-									</div>
-								</div>
-								<div className="row">
-									<div className="ui grid container">
-										<p><b>Border Countries: </b></p>
-										{this.renderBorderLinks()}
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  joinLanguages = languages => {
+    if (languages) {
+      const languagesString = [];
+      languages.forEach(language => {
+        languagesString.push(language.name);
+      });
+      return languagesString.join(', ');
+    }
+  };
+
+  componentDidMount() {
+    if (!this.props.country.name) {
+      this.props.history.push('/');
+    }
+  }
+
+  componentDidUpdate() {
+    if (!this.props.country.name) {
+      this.props.history.push('/');
+    }
+  }
+
+  render() {
+    return <div>{this.renderInfo()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    country: state.countries.selectedCountry,
+    borderCountries: state.countries.borderCountries,
+    display: state.interface.display,
+  };
 };
 
-const mapStateToProps = (state) => {
-	return {
-		country: state.countries.selectedCountry,
-		borderCountries: state.countries.borderCountries,
-		display: state.interface.display
-	};
-};
-
-export default connect(mapStateToProps, { fetchCountriesByCode, selectCountry })(CountryShow);
+export default connect(
+  mapStateToProps,
+  { fetchCountriesByCode, selectCountry }
+)(CountryShow);
